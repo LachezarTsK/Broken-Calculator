8412862ed5cafb89f4c0c8177ba3fc83f534a5e8
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-03-25 06:38:20 +0100

    Create Solution.cpp

---
+++ Solution.cpp
@@ -0,0 +1,22 @@
+
+using namespace std;
+
+class Solution {
+    
+public:
+
+    int brokenCalc(int startValue, int targetValue) {
+
+        int numberOfOperations = 0;
+        while (startValue < targetValue) {
+            if (targetValue % 2 == 0) {
+                targetValue /= 2;
+            } else {
+                targetValue++;
+            }
+            numberOfOperations++;
+        }
+
+        return numberOfOperations + startValue - targetValue;
+    }
+};
